8037e802ade1c0afa8ca64040ce40d204b3d81cc
Jacopo Piazzalunga <15248311+Jacopopiazza@users.noreply.github.com>
2024-01-19 14:14:43 +0100

    Added Spring Backend

---
+++ ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/CodeKataBattleApplication.java
@@ -0,0 +1,13 @@
+package com.polimi.PPP.CodeKataBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeKataBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeKataBattleApplication.class, args);
+	}
+
+}
---
+++ ITD/BACKEND/CodeKataBattle/src/test/java/com/polimi/PPP/CodeKataBattle/CodeKataBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polimi.PPP.CodeKataBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeKataBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
