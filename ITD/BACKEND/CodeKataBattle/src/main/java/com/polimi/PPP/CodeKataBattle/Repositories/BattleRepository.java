4c77c1805a2161c360edff0d8a6bb7059c34f354
peetceenatoo <piccinatomattia@gmail.com>
2024-01-27 12:43:17 +0100

    Sorry guys for injecting bugs again, please understand my situation, it upsets me as well

--- ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/Repositories/BattleRepository.java
+++ ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/Repositories/BattleRepository.java
@@ -2,6 +2,8 @@ package com.polimi.PPP.CodeKataBattle.Repositories;
 
 import com.polimi.PPP.CodeKataBattle.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -9,6 +11,10 @@ import java.util.List;
 public interface BattleRepository extends JpaRepository<Battle, Long> {
     List<Battle> findByTournamentId(Long tournamentId);
 
-    @Query("SELECT b FROM Battle b JOIN b.battleSubscriptions bs WHERE b.tournament.id = :tournamentId AND bs.user.id = :userId")
+    @Query("SELECT b FROM Battle b " +
+            "JOIN b.tournament t " +
+            "JOIN BattleSubscription bs ON bs.battle = b " +
+            "WHERE t.id = :tournamentId AND bs.user.id = :userId")
     List<Battle> findBattlesByTournamentIdAndUserId(@Param("tournamentId") Long tournamentId, @Param("userId") Long userId);
+
 }
