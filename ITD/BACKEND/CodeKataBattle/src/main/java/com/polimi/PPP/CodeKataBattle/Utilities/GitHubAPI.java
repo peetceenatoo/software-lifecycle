673d2c8f92dc877caa14d997f15716d848c90920
peetceenatoo <piccinatomattia@gmail.com>
2024-01-29 16:04:02 +0100

    POST CreateBattle goes brrr (I'm not confident that it works)

--- ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/Utilities/GitHubAPI.java
+++ ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/Utilities/GitHubAPI.java
@@ -55,6 +55,12 @@ public class GitHubAPI implements IGitHubAPI {
         }catch (Exception e){
             throw new ErrorInConnectingToGitHubException("Error in creating repository");
         }
+        try{
+        repository.addCollaborators(gitHub.getUser("GabP404"));
+        }catch (Exception e){
+            throw new ErrorInConnectingToGitHubException("Error in adding collaborator");
+        }
+
 
         return repository.getHtmlUrl().toString();
     }
