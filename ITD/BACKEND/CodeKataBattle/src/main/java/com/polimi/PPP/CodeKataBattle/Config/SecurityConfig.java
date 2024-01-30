c224bda5b727f9e135bc1d0617b591c4fcc5138d
Jacopo Piazzalunga <15248311+Jacopopiazza@users.noreply.github.com>
2024-01-30 17:14:51 +0100

    fix failing test

--- ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/Config/SecurityConfig.java
+++ ITD/BACKEND/CodeKataBattle/src/main/java/com/polimi/PPP/CodeKataBattle/Config/SecurityConfig.java
@@ -23,7 +23,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@Profile("!test")
 public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsService;
