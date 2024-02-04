b78890dbba73aa9425b951fc84356727e9c81f26
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2024-02-04 20:26:41 +0100

    fixed Header navlinks

--- Frontend/codekatabattle-app/src/components/Header.jsx
+++ Frontend/codekatabattle-app/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import {onclickButtonNotYetImplemented} from '../utilities/alerting'
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/');
+  }
+
   return (
     <Navbar bg="primary" variant="dark" expand="lg" className="px-0">
       <Container fluid className="px-0">
@@ -10,7 +18,7 @@ const Header = () => {
           <Col md={10} lg={10}>
             <Row>
               <Col xs={12} lg={3}>
-                <Navbar.Brand href="/home">CodeKataBattle</Navbar.Brand>
+                <Navbar.Brand>CodeKataBattle</Navbar.Brand>
               </Col>
               <Col xs={12} lg={5} className="d-flex align-items-center justify-content-center">
                 <Form className='d-flex flex-row justify-content-center' >
@@ -18,15 +26,15 @@ const Header = () => {
                     <FormControl type="text" placeholder="Search" style={{ width: 'auto' }} />
                   </Col>
                   <Col xs={12} lg={6} className="d-flex align-items-center justify-content-center">
-                  <Button variant="outline-light">Search</Button>
+                  <Button variant="outline-light" onClick={onclickButtonNotYetImplemented}>Search</Button>
                   </Col>
                 </Form>
               </Col>
               <Col xs={12} lg={4} className="d-flex justify-content-end">
                 <Nav>
-                  <Nav.Link href="#welcome">Welcome {localStorage.getItem('username')}</Nav.Link>
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#profile">Profile</Nav.Link>
+                  <Nav.Link>Welcome {localStorage.getItem('username')}</Nav.Link>
+                  <Nav.Link onClick={goToHome}>Home</Nav.Link>
+                  <Nav.Link onClick={onclickButtonNotYetImplemented}>Profile</Nav.Link>
                 </Nav>
               </Col>
             </Row>
